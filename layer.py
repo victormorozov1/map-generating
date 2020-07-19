48c1ed95b9e42926adde682a5234b1af70362f8b
victormorozov1 <vismo@bk.ru>
2020-07-19 23:01:24 +0500

    class Layer is ready

--- layer.py
+++ layer.py
@@ -22,17 +22,25 @@ class Layer:
             for j in range(self.n):
                 self.arr[-1].append(Color(color_range=self.color_range))
 
+    def parabola(self, x):
+        return x ** 2 * (0.5 / 0.25)
+
+    def double_parabola(self, x):
+        if x < 0.5:
+            return self.parabola(x)
+        return 1 - self.parabola(1 - x)
+
     def f(self, v1, v2, k):
+        k = self.double_parabola(k)
         return v1 * (1 - k) + v2 * k
 
     def pixels(self, i, j):
         arr_i = i // self.cell_sz
         arr_j = j // self.cell_sz
-        print(self.n, arr_i, arr_j)
+
         val_up = self.f(self.arr[arr_i][arr_j], self.arr[arr_i][arr_j + 1], j % self.cell_sz / self.cell_sz)
         val_down = self.f(self.arr[arr_i + 1][arr_j], self.arr[arr_i + 1][arr_j + 1], j % self.cell_sz / self.cell_sz)
 
-        print(self.f(val_up, val_down, i % self.cell_sz / self.cell_sz))
         return self.f(val_up, val_down, i % self.cell_sz / self.cell_sz)
 
     def show(self):
@@ -51,9 +59,11 @@ if __name__ == '__main__':
     pygame.init()
     win = pygame.display.set_mode((1000, 1000))
 
-    layer = Layer(512, 5, (256, 256, 256), win)
+    layer = Layer(512, 17, (100, 100, 100), win)
     layer.show()
     input()
+
+
 
 
 
