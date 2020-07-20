98dc682a64c9d3a7d0ca41b25188926d2516af3a
victormorozov1 <vismo@bk.ru>
2020-07-20 18:34:23 +0500

    fix bag with map.range

--- map.py
+++ map.py
@@ -1,12 +1,12 @@
+import os
+from PIL import Image, ImageDraw
 from color_class import Color
 from layer_group import LayerGroup
-import pygame
 
 
 class Map:
-    def __init__(self, sz, win, range=1000, water_level=500, snow_level=800):
+    def __init__(self, sz, range=100000, water_level=50000, snow_level=80000):
         self.sz = sz
-        self.win = win
         self.range = range
         self.water_level = water_level
         self.snow_level = snow_level
@@ -15,38 +15,38 @@ class Map:
         self.clouds = LayerGroup(22, sz, range)
 
     def show(self):
+        im = Image.new('RGBA', (self.sz, self.sz), 'black')
+        pixels = im.load()
+
         for i in range(self.sz):
             print(i / self.sz * 100, '%')
             for j in range(self.sz):
                 h = self.landscape[i, j]
                 global color
                 if h < self.water_level:  # Water
-                    color = Color(0, 0, h / 2)
+                    color = Color(0, 0, h / 2 * 1000 / self.range)
                 elif h < self.snow_level:
-                    color = Color(h / 10, h / 4, h / 30)
+                    color = Color(h * 100 / self.range, h / 4 * 1000 / self.range, h / 30 * 1000 / self.range)
                 else:
-                    color = Color(h / 8 + 128, h / 8 + 128, h / 4)
+                    c = h / 8 * 1000 / self.range + 128
+                    color = Color(c, c, c)
                 color *= 0.2
 
                 # Clouds
-                h = (self.clouds[i, j] / 1000) ** 3 * 1000
-                color = color + Color(h / 4, h / 4, h / 4) * 0.8
+                h = (self.clouds[i, j] / self.range) ** 3 * self.range
+                color = (color + Color(h / 4 * 1000 / self.range, h / 4 * 1000 / self.range, h / 4 * 1000 / self.range) * 0.8).tuple()
+                pixels[i, j] = int(color[0]), int(color[1]), int(color[2])
 
-                pygame.draw.rect(self.win, color.tuple(), [i, j, 1, 1])
+        im.show()
 
-        """for i in range(self.sz):
-            print(50 + i / self.sz * 50, '%')
-            for j in range(self.sz):
-                h = self.clouds[i, j]
-                if h > 600:
-                    pygame.draw.rect(self.win, (100 + h / 7, 100 + h / 7, 10 + h / 7), [i, j, 1, 1]"""
-        pygame.display.update()
+        arr = [int(filename.split('_')[0]) for filename in filter(lambda name: '_' in name, os.listdir('map_results'))]
+        n = 1
+        if arr:
+            n += max(arr)
+        im.save(f'map_results/{n}_map.png')
 
 
 if __name__ == '__main__':
-    pygame.init()
-    win = pygame.display.set_mode((1111, 1111))
-
-    map = Map(1024, win)
+    map = Map(2048)
     map.show()
     input()
